f18f5767a62ba3a8c84d83b59d5c6bbe78c1b576
yjl24744 <yjl24744@163.com>
2020-06-28 15:44:53 +0800

    增加消费者接口调用

--- payment/src/main/java/com/yang/payment/rest/PayController.java
+++ payment/src/main/java/com/yang/payment/rest/PayController.java
@@ -1,6 +1,5 @@
 package com.yang.payment.rest;
 
-import com.yang.api.entity.vo.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
